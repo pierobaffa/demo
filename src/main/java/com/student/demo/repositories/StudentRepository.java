93b7340a73793d6e0707d452500f5bfcf3111876
AzureAD\PiercosimoNicolòBaff <Piercosimo Nicolo Baffa>
2022-10-06 18:00:59 +0200

    fix Repository

--- src/main/java/com/student/demo/repositories/StudentRepository.java
+++ src/main/java/com/student/demo/repositories/StudentRepository.java
@@ -2,10 +2,11 @@ package com.student.demo.repositories;
 
 import com.student.demo.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    List<Student> findByNameAndLastName(String firstName, String lastName);
+    List<Student> findByFirstNameAndLastName(String firstName, String lastName);
 }
